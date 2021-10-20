5f1e7afd7b509bac85dc9e546bcbf17f663e70d3
Enderset123 <86422885+Enderset123@users.noreply.github.com>
2021-10-20 13:29:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var speechRecognition=window.webkitSpeechRecognition;
+var recognition=new speechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML="";
+    //calling a pre-defined function start of speechRecognitionAPI
+    recognition.start();
+}
+recognition.onresult=function run (event){
+    console.log(event);
+
+    var content=event.results[0][0].transcript;
+    console.log(content);
+
+    document.getElementById("textbox").innerHTML=content;
+    if(content=="take my selfie"){
+        console.log("taking Selfie")
+        speak();
+    }
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+
+    speak_data="taking your selfie in 5 seconds"
+
+    var utterthis=new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterthis);
+    Webcam.attach(camera)
+
+    setTimeout(function(){
+        snapshot();
+        save();
+    },5000)
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:"png",
+    png_quality:90
+})
+camera=document.getElementById("camera")
+
+function snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+ data_uri+'"/>';
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
+}
